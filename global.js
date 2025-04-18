ff3b7d88352ab866a29a632d26d53fc2a640fe72
Jacquelyn Garcia <160792688+JacquelynGarcia@users.noreply.github.com>
2025-04-17 23:12:13 -0700

    removing hard coded navs

--- global.js
+++ global.js
@@ -1,13 +1,34 @@
 console.log('IT’S ALIVE!');
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
-}
-
-const navLinks = $$('nav a');
-
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-
-currentLink?.classList.add('current');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'about/', title: 'About' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/yourusername', title: 'GitHub' },
+  ];
+  
+  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/" 
+    : "/portfolio/";
+  
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  
+  for (let p of pages) {
+    let url = !p.url.startsWith('http') ? BASE_PATH + p.url : p.url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = p.title;
+  
+    
+    a.classList.toggle(
+      'current',
+      a.host === location.host && a.pathname === location.pathname
+    );
+  
+    
+    a.target = (a.host !== location.host) ? "_blank" : "_self";
+  
+    nav.append(a);
+  }
